0fd21cc225ef515f231f38c823a69b0d249e0c37
Prateek-Rath <prateekrath2023@gmail.com>
2026-04-14 01:00:19 +0530

    some more func, will do fwd pass...

--- nn.ml
+++ nn.ml
@@ -1,4 +1,15 @@
 open Matrix
+module M = Matrix
+
+(* between layers we have weights and biases *)
+type layer = {
+        w: matrix; (* n_l * n_(l+1) matrix *)
+        b: matrix; (* 1 x  N matrix *)
+}
+
+(* a model is a list of layers *)
+type model = layer list
+
 
 (** Activation Functions **)
 
@@ -12,6 +23,16 @@ let relu m = map relu_fn m
 
 let tanh m = map (fun x -> tanh x) m
 
+let softmax_vector v =
+        let max_val = List.fold_left max (-1e9) v in 
+        let exps = List.map (fun x -> exp (x -. max_val)) v in (* this prevents div by very small number *)
+        let sum_exps = List.fold_left (+.) 0.0 exps in 
+        List.map (fun x -> x /. sum_exps) exps 
+
+let softmax m = 
+        List.map (fun row -> softmax_vector row) m
+
+
 (** Derivatives **)
 
 (* sigmoid_derivative expects the output of the sigmoid function (post-activation) *)
@@ -24,6 +45,14 @@ let relu_derivative pre_act =
 (* tanh_derivative expects the output of the tanh function (post-activation) *)
 let tanh_derivative post_act =
   map (fun t -> 1.0 -. (t *. t)) post_act
+
+(* we won't need softmax derivative *)
+(* 
+
+        while using softmax + cross-entropy, the derivative simplifies to:
+        gradient = pred − target
+
+*)
 
 (** Loss Functions **)
 
@@ -57,3 +86,30 @@ let bce_derivative pred target =
     let p = if p < epsilon then epsilon else if p > 1.0 -. epsilon then 1.0 -. epsilon else p in
     (1.0 /. n) *. (p -. t) /. (p *. (1.0 -. p))
   ) pred target
+  
+(* Categorical Cross Entropy: returns a single float scalar *)
+let cross_entropy pred target =
+  let epsilon = 1e-15 in
+  let losses = map2 (fun p t ->
+    let p =
+      if p < epsilon then epsilon
+      else if p > 1.0 -. epsilon then 1.0 -. epsilon
+      else p
+    in
+    -. (t *. log p)
+  ) pred target in
+  let flat = flatten losses in
+  let sum = List.fold_left (+.) 0.0 flat in
+  sum /. (float_of_int (List.length pred))
+
+let cross_entropy_derivative pred target =
+  let n = float_of_int (List.length pred) in
+  map2 (fun p t ->
+    (p -. t) /. n
+  ) pred target
+
+
+(** Forward Pass **)
+(* take in model i.e a list of layers and input *)
+(* returns (activations, z_values) in reverse order for backprop *)
+        
--- nn.mli
+++ nn.mli
@@ -1,15 +1,23 @@
-open Matrix
+type layer
 
-val sigmoid : matrix -> matrix
-val relu : matrix -> matrix
-val tanh : matrix -> matrix
+type model
 
-val sigmoid_derivative : matrix -> matrix
-val relu_derivative : matrix -> matrix
-val tanh_derivative : matrix -> matrix
 
-val mse : matrix -> matrix -> float
-val mse_derivative : matrix -> matrix -> matrix
+val sigmoid : Matrix.matrix -> Matrix.matrix
+val softmax: Matrix.matrix -> Matrix.matrix
+val relu : Matrix.matrix -> Matrix.matrix
+val tanh : Matrix.matrix -> Matrix.matrix
 
-val bce : matrix -> matrix -> float
-val bce_derivative : matrix -> matrix -> matrix
+val sigmoid_derivative : Matrix.matrix -> Matrix.matrix
+val relu_derivative : Matrix.matrix -> Matrix.matrix
+val tanh_derivative : Matrix.matrix -> Matrix.matrix
+
+val mse : Matrix.matrix -> Matrix.matrix -> float
+val mse_derivative : Matrix.matrix -> Matrix.matrix -> Matrix.matrix
+
+val bce : Matrix.matrix -> Matrix.matrix -> float
+val bce_derivative : Matrix.matrix -> Matrix.matrix -> Matrix.matrix
+
+val cross_entropy : Matrix.matrix -> Matrix.matrix -> float
+val cross_entropy_derivative : Matrix.matrix -> Matrix.matrix -> Matrix.matrix
+
